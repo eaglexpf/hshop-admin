8fc706ad5a17b6ed29cb00a87b909ab044e2b740
eaglexpf <1121252990@qq.com>
2023-07-06 17:35:42 +0800

    add email validate

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -15,15 +15,20 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validUsername(str) {
+  // 验证手机号
   let reg = /^1[3456789]\d{9}$/
   // ^1  以1开头
   // [3456789] 第2位，使用原子表里的任意一个原子都可以
   // \d{9}$  第三位  朝后可以是任意数字  并且最后结尾必须是数字
-  console.log(reg.test(str))
   if (reg.test(str)) {
     return true
   }
+  // 验证邮箱
+  reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  if (reg.test(str)) {
+    return true
+  }
+  // 验证账号
   reg = /^[a-zA-z]\w{3,15}$/
-  console.log(reg.test(str))
   return reg.test(str)
 }
